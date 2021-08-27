4eaa934ceccd19963e57fb42db9be4ae0d8e9750
ClassmateLin <Yue.Lin@pccw.com>
2021-08-27 10:19:28 +0800

    添加女装盲盒定时任务

--- jd_women_box.py
+++ jd_women_box.py
@@ -3,6 +3,7 @@
 # @Time    : 2021/8/27 10:00 上午
 # @File    : jd_women_box.py
 # @Project : jd_scripts
+# @Cron    : 5 1 * * *
 # @Desc    : 女装盲盒
 from utils.jd_anmp import JdAnmp
 from utils.process import process_start
